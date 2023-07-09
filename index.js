ac9fd3f626f9dd233bcee5535e085c15d980a335
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-07-09 11:33:47 +0100

    fix layout

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 
 
-
-const imageContainer = document.querySelector('.image-container');
-
-imageContainer.addEventListener('mousemove', (e) => {
-  const containerRect = imageContainer.getBoundingClientRect();
-  const mouseX = e.clientX - containerRect.left;
-
-  const images = imageContainer.querySelectorAll('img');
-  images.forEach((img) => {
-    const imageRect = img.getBoundingClientRect();
-    const imageWidth = imageRect.width;
-    const translateX = ((mouseX / containerRect.width) - 0.5) * imageWidth;
-    img.style.transform = `translateX(${translateX}px)`;
-  });
+const imagesContainer = document.querySelector('.images-container');
+imagesContainer.addEventListener('mouseenter', () => {
+  imagesContainer.classList.remove('hidden');
 });
+imagesContainer.addEventListener('mouseleave', () => {
+  imagesContainer.classList.add('hidden');
+});
+
+document.addEventListener('click', function(){console.log('clicked')})
